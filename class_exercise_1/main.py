f607e4eea5a38326dd5f4767936fd49ce1610940
JosuEndara <sasdexl@gmail.com>
2022-09-01 00:11:15 -0500

    intento 1

---
+++ class_exercise_1/main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def sum(inp):
+    suma=0
+    for i in range(1,inp+1):
+        suma+=i
+    return suma
+
+num=sum(5)
+print(num)
